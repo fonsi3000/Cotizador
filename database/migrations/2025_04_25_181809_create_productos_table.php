7fa8ff5a9bc3d79d2dcf4a9656c13788c540500c
fonsi3000 <luiiscarrascal@gmail.com>
2025-05-22 09:36:39 -0500

    Se quita la validacion unica para los productos

--- database/migrations/2025_04_25_181809_create_productos_table.php
+++ database/migrations/2025_04_25_181809_create_productos_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('productos', function (Blueprint $table) {
             $table->id();
-            $table->string('codigo');
+            $table->string('codigo')->unique();
             $table->string('descripcion');
             $table->string('imagen')->nullable();
             $table->boolean('activo')->default(true);
